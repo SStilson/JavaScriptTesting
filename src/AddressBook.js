073dd79b29d13da54ff4ef9d3fbda2beceff7be6
SStilson <sstilson@hmc.edu>
2015-07-21 19:38:44 -0700

    Finished testing framework

---
+++ src/AddressBook.js
@@ -0,0 +1,25 @@
+var AddressBook = function() {
+	this.contacts = [];
+}
+
+AddressBook.prototype.getInitialContacts = function(cb) {
+	var self = this;
+	setTimeout(function() {
+		self.initialComplete = true;
+		if(cb) {
+			return cb();
+		}
+	}, 3);
+}
+
+AddressBook.prototype.addContact = function(contact) {
+	this.contacts.push(contact);
+}
+
+AddressBook.prototype.getContact = function(num) {
+	return this.contacts[num];
+}
+
+AddressBook.prototype.deleteContact = function(num) {
+	this.contacts.splice(num, 1);
+}
